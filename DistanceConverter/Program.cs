b60a95bde4ef86bcfb971e2987f41d1b678d4125
一貴 大友 <32014@ojs.ac.jp>
2021-05-21 15:53:03 +0900

    P49 リスト2,4

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -8,18 +8,26 @@ namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
             if (args.Length >= 1 && args[0] == "-tom") {
-                for (int feet = 1; feet <= 10; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
-                }
+                //フィートからメートルへの対応表を出力
+                PrintFeetToMeterList(0, 10);
+            }else {
+                //メートルからフィートへの対応表を出力
+                PrintMeterToFeetList(0, 10);
             }
-            else {
-                for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine("{0} m = {1:0.0000}ft", meter, feet);
-                }
+        }
+        //メートルからフィートへの対応表を出力
+        private static void PrintMeterToFeetList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0} m = {1:0.0000}ft", meter, feet);
             }
-
+        }
+        //メートルからフィートへの対応表を出力
+        private static void PrintFeetToMeterList(int start,int stop) {
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
+            }
         }
 
         private static double FeetToMeter(int feet) {
