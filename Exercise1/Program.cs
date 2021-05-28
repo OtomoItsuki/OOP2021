e67a19b6c4a820efefba2ca82b1b8d6e9f17f6e7
一貴 大友 <32014@ojs.ac.jp>
2021-05-28 14:44:29 +0900

    P79 2.1 配列songsにインスタンスを格納

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
+            var songs = new Song[3];
+            songs[0] = new Song("Rythm", "Bird", 300);
+            songs[1] = new Song("Dance", "Monkey", 192);
+            songs[2] = new Song("Visual", "Dog", 270);
         }
     }
 }
