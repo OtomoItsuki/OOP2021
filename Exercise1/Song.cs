35311ba6fe3ff34800a8126ba89108c177f27568
一貴 大友 <32014@ojs.ac.jp>
2021-05-28 14:29:07 +0900

    P79 2.1 Songクラス プロパティ コンストラクタ作成

--- Exercise1/Song.cs
+++ Exercise1/Song.cs
@@ -6,5 +6,14 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Song {
+        string Title;
+        string ArtistName;
+        int Length;
+
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
     }
 }
