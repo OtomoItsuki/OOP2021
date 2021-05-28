7358e2d633b390b80e889cee6a43ccad062b5b69
一貴 大友 <32014@ojs.ac.jp>
2021-05-28 15:15:32 +0900

    P79 2.1 格納したSongオブジェクトの内容をコンソールにすべて表示 完成版

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,10 +7,21 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
-            var songs = new Song[3];
+            int songsNumber = 3;
+            
+            var songs = new Song[songsNumber];
+
             songs[0] = new Song("Rythm", "Bird", 300);
             songs[1] = new Song("Dance", "Monkey", 192);
             songs[2] = new Song("Visual", "Dog", 270);
+
+            printSongs(songs, songsNumber);
+        }
+        static void printSongs(Song[] Songs,int songsNumber) {
+            foreach (var Song in Songs) {
+                Console.WriteLine(Song.printSong());
+            }
         }
     }
+    
 }
--- Exercise1/Song.cs
+++ Exercise1/Song.cs
@@ -6,14 +6,26 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Song {
-        string Title;
-        string ArtistName;
-        int Length;
+        private string Title;
+        private string ArtistName;
+        private int Length;
 
         public Song(string title, string artistName, int length) {
             Title = title;
             ArtistName = artistName;
             Length = length;
         }
+        public string printSong() {
+            string remainder;
+            if (Length % 60 != 0) {
+                remainder = Length % 60 + "";
+            }
+            else {
+                remainder = "00";
+            }
+            String songInfo = "題名：" + Title + ",作者：" + ArtistName + ",演奏時間：" + Length / 60 + ":" + remainder;
+            return songInfo;
+        }
+        
     }
 }
