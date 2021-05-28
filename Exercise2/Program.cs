ce641a5754dea9e2a7dc81020ac321b52401f159
一貴 大友 <32014@ojs.ac.jp>
2021-05-28 15:37:11 +0900

    P79 2.2 Exercise2の作成、インチからメートルへの変換をするクラスを作成

---
+++ Exercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            PrintInchList(1, 10);
+        }
+
+        private static void PrintInchList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+
+            }
+        }
+    }
+}
