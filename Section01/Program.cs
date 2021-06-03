4e1fe76611927f02a391db2dac67439329687410
一貴 大友 <32014@ojs.ac.jp>
2021-06-03 15:39:25 +0900

    P99 3.1.2 ForEachメソッドでnumbersを出力

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -7,29 +7,43 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            //var names = new List<string> {
-            //    "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
-            //};
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
 
-            //IEnumerable<string> query = names.Where(s => s.Length <= 5)
-            //                            .Select(s=>s.ToLower());
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
 
-            //foreach (string s in query) 
-            //    Console.WriteLine(s);
-            //
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
 
-            string[] names = {
-                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra"  };
-            var query = names.Where(s => s.Length <= 5).ToList();
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
 
-            query.ForEach(Console.WriteLine);
-            foreach (var item in query)
-                Console.WriteLine(item);
-            Console.WriteLine("------");
+            // 3.1.4
+            Exercise1_4(numbers);
 
-            names[0] = "Osaka";
-            foreach (var item in query)
-                Console.WriteLine(item);
+        }
+
+        private static void Exercise1_1(List<int> numbers) {
+            var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            if (exist)
+                Console.WriteLine("存在しています");
+            else
+                Console.WriteLine("存在していません");
+        }
+
+        private static void Exercise1_2(List<int> numbers) {
+            numbers.ForEach(n => Console.WriteLine(n/2));
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+            throw new NotImplementedException();
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+            throw new NotImplementedException();
         }
     }
 }
