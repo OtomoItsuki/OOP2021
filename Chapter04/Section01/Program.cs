30d93c869113ada746696522855f96ea63a5a095
一貴 大友 <32014@ojs.ac.jp>
2021-06-08 11:05:22 +0900

    P116 リスト4.27 null合体演算子

--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -7,6 +7,29 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+
+            //var person = new Person {
+            //    Name = "新井遥奈",
+            //    Birthday = new DateTime(1995, 11, 23),
+            //    PhoneNumber = "123-3456-7890",
+            //};
+
+            //var list = new List<int> { 10, 20, 30, 40, };
+            //var key = 30;
+            //var num = list.Contains(key) ? 1 : 0;
+            //Console.WriteLine(num);
+
+            string code = "12345";
+            //null合体演算子
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);
+        }
+        //スタブ
+        private static object DefaultMessage() {
+            return "Default Message";
+        }
+        private static object GetMessage(object code) {
+            return code;
         }
     }
 }
