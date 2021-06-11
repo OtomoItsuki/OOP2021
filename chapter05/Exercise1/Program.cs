61a850edce137ec7c3e5d658beb5fb12adf77e0b
一貴 大友 <32014@ojs.ac.jp>
2021-06-11 15:55:04 +0900

    chapter05 問題5.2 完成

--- chapter05/Exercise1/Program.cs
+++ chapter05/Exercise1/Program.cs
@@ -7,6 +7,30 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
+#if false
+            #region 問題5.1
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+            if (String.Compare(s1, s2, ignoreCase: true) == 0) {
+                Console.WriteLine("等しい");
+            }
+            else {
+                Console.WriteLine("等しくない");
+            }
+            Console.WriteLine("-----");
+            #endregion
+            #region 問題5.2
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n.ToString("#,0"));
+            Console.WriteLine("-----");
+            #endregion
+
+#endif
+            #region 問題5.3
+            //var s = "Jackdaws love my big sphinx of quartz";
+            #endregion
+
         }
+
     }
 }
