4592f0529a6b13e6cf93d21e7e1b4af14bca155d
一貴 大友 <32014@ojs.ac.jp>
2021-06-21 14:31:03 +0900

    完成

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,37 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
 
-
-
-            
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要： テストのデータを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            var students = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                var student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(student);
+            }
+            return students;
         }
 
-        //メソッドの概要： 
+        //メソッドの概要： 科目別点数を求める
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
-            
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Subject))
+                    dict[score.Subject] += score.Score;
+                else
+                    dict[score.Subject] = score.Score;
+            }
+            return dict;
         }
     }
 }
