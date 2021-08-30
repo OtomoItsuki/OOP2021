6fe675286d2602174c365ff667a796059094d2ed
一貴 大友 <32014@ojs.ac.jp>
2021-08-30 16:02:56 +0900

    11.2　未完成

---
+++ Chapter11/Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            var xdoc = XDocument.Load("11_2.xml");
+
+            var dkanji = xdoc.Root.Elements();
+
+            var word = new XElement(xdoc.Root.Name);
+
+            var root = new XElement(xdoc.Root.Name, word);
+
+            Console.WriteLine();
+        }
+    }
+}
