81caf90873af5d4a7dec3523c5776ce241f306bf
一貴 大友 <32014@ojs.ac.jp>
2021-08-30 15:07:18 +0900

    Execise1_4　完成

--- Chapter11/Exercise1/Program.cs
+++ Chapter11/Exercise1/Program.cs
@@ -18,11 +18,16 @@ namespace Exercise1 {
             Console.WriteLine("-11.1.3-----");
             Exercise1_3(file);
 
+            Console.WriteLine("-11.1.4-----");
+            Exercise1_4(file);
+
             Console.WriteLine("------------");
         }
+
         //11.1.1
         private static void Exercise1_1(string file) {
             var xdoc = XDocument.Load(file);
+
             var xelements = xdoc.Root.Elements();
             foreach (var xballsport in xelements) {
                 XElement xname = xballsport.Element("name");
@@ -34,11 +39,13 @@ namespace Exercise1 {
         //11.1.2
         private static void Exercise1_2(string file) {
             var xdoc = XDocument.Load(file);
+
             var ballsports = xdoc.Root.Elements().Select(x => new {
                         FirstPlayed = (int)x.Element("firstplayed"),
                         KanjiName = (string)x.Element("name").Attribute("kanji"),
             
             }).OrderByDescending(x => x.FirstPlayed);
+
             foreach (var ballsport in ballsports) {
                 Console.WriteLine("最初にプレーされた年：{0}：漢字の競技名：{1}", ballsport.FirstPlayed, ballsport.KanjiName);
             }
@@ -48,15 +55,30 @@ namespace Exercise1 {
         //11.1.3
         private static void Exercise1_3(string file) {
             var xdoc = XDocument.Load(file);
+
             var ballsport = xdoc.Root.Elements().Select(x => new {
-                TeamMembers = (int)x.Element("teammembers"),
-                Name = (string)x.Element("name")
+                    TeamMembers = (int)x.Element("teammembers"),
+                    Name = (string)x.Element("name")
 
             }).OrderByDescending(x => x.TeamMembers).First();
-            ;
-            Console.WriteLine("メンバー数：{0}　競技名：{1}", ballsport.TeamMembers, ballsport.Name);
             
+            Console.WriteLine("メンバー数：{0}人　競技名：{1}", ballsport.TeamMembers, ballsport.Name);
+
 
         }
+        private static void Exercise1_4(string file) {
+            var newFile = "sports.xml";
+            
+            var element = new XElement("ballsport",
+                             new XElement("name", "サッカー", new XAttribute("kanij", "蹴球")),
+                             new XElement("teammembers", 11),
+                             new XElement("firstplayed", 1863)
+                          );
+            var xdoc = XDocument.Load("Sample.xml");
+
+            xdoc.Root.Add(element);
+            xdoc.Save(newFile);
+        }
+
     }
 }
