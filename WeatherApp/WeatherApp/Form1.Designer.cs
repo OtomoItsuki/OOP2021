ff02ae54285bea25e067a75fa0080e085d5e2130
一貴 大友 <32014@ojs.ac.jp>
2021-09-03 15:24:28 +0900

    演習2  完成

---
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbArea = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.tbOtherInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地域を選択";
+            // 
+            // cbArea
+            // 
+            this.cbArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbArea.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbArea.FormattingEnabled = true;
+            this.cbArea.Items.AddRange(new object[] {
+            "前橋",
+            "みなかみ",
+            "水戸",
+            "宇都宮",
+            "その他"});
+            this.cbArea.Location = new System.Drawing.Point(105, 32);
+            this.cbArea.Name = "cbArea";
+            this.cbArea.Size = new System.Drawing.Size(183, 21);
+            this.cbArea.TabIndex = 1;
+            this.cbArea.SelectedIndexChanged += new System.EventHandler(this.cbArea_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(448, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "天気表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbResult.Location = new System.Drawing.Point(31, 83);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(501, 408);
+            this.tbResult.TabIndex = 3;
+            // 
+            // tbOtherInput
+            // 
+            this.tbOtherInput.BackColor = System.Drawing.SystemColors.Info;
+            this.tbOtherInput.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbOtherInput.Location = new System.Drawing.Point(311, 32);
+            this.tbOtherInput.Name = "tbOtherInput";
+            this.tbOtherInput.Size = new System.Drawing.Size(100, 20);
+            this.tbOtherInput.TabIndex = 4;
+            this.tbOtherInput.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(570, 513);
+            this.Controls.Add(this.tbOtherInput);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbArea);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbArea;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.TextBox tbOtherInput;
+    }
+}
+
---
+++ WeatherApp/WeatherApp/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WeatherApp {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            //選択しているものが何もない状態がないようにする
+            cbArea.SelectedIndex = 0;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) {
+            //一番最初にリセットをすることで前の結果と重なって見づらくなることを防ぐ
+            tbResult.Text = "";
+            
+            int code = 0;
+            
+            switch (cbArea.SelectedIndex) {
+                case 0:
+                    code = 4210;
+                    break;
+                case 1:
+                    code = 4220;
+                    break;
+                case 2:
+                    code = 4110;
+                    break;
+                case 3:
+                    code = 4010;
+                    break;
+                case 4:
+                    int.TryParse(tbOtherInput.Text, out code);
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            var results = GetWeatherReportFromYahoo(code);
+            foreach (var s in results) {
+
+                tbResult.Text += s +"\r\n";
+            }
+
+        }
+
+        private void cbArea_SelectedIndexChanged(object sender, EventArgs e) {
+            if(cbArea.SelectedIndex == 4) {
+                MessageBox.Show("表示したい地域のコードを\r\n黄色のテキストボックスに入力してください",
+                    "表示したい地域のコードを入力してください", MessageBoxButtons.OK);
+
+                //tbOtherInputがfalseならtrueにする(いるのかわからないがとりあえず)
+                if (!tbOtherInput.Visible) {
+                    tbOtherInput.Visible = true;
+                }
+            }
+            else {
+                if (tbOtherInput.Visible) {
+                    tbOtherInput.Visible = false;
+                }
+            }
+        }
+
+        private static IEnumerable<string> GetWeatherReportFromYahoo(int cityCode) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(
+                    @"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml", cityCode);
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes) {
+                    string s = Regex.Replace(node.Value, "【|】", "");
+                    yield return s;
+                }
+            }
+        }
+    }
+}
