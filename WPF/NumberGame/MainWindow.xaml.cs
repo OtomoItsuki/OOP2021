f5b559bdf2938246ee299f3132a78fca68e6c668
一貴 大友 <32014@ojs.ac.jp>
2021-11-17 15:43:58 +0900

    NumberGame 改良版 数字の量を可変に ボタンの生成方法の変更など

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -18,15 +18,21 @@ namespace NumberGame {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
-        private string Anser;
+        private int Anser;
         int clickCount;
+
+        int gridRow = 10;
+        int gridColumn = 10;
+
+        Random rand = new Random();
+        List<Button> buttons = new List<Button>();
+        SolidColorBrush solidButtonColor = new SolidColorBrush(Colors.Yellow);
+        SolidColorBrush hitButtonColor = new SolidColorBrush(Colors.Red);
         public MainWindow() {
             InitializeComponent();
-            Random rand = new Random();
-            Anser = rand.Next(1, 26).ToString();
-            Button bt1 = new Button();
-            bt1.Content = "テスト";
+
             
+
         }
 
 
@@ -34,13 +40,51 @@ namespace NumberGame {
             Button button = (Button)sender;
             clickCount++;
             lbclickCount.Content = "クリック数:" + clickCount;
-            if (button.Content.ToString() == Anser) {
+            if (int.Parse(button.Content.ToString()) == Anser) {
                 lbclearTxt.Content = "正解:" + Anser;
+                buttons[Anser].SetValue(BackgroundProperty, hitButtonColor);
                 MessageBox.Show("正解!");
             }
             else {
                 lbclearTxt.Content = "はずれ";
+                buttons[int.Parse(button.Content.ToString()) - 1].SetValue(BackgroundProperty, solidButtonColor);
+
+                if (int.Parse(button.Content.ToString()) > Anser) {
+
+                    lbclearTxt.Content += "　これより低いです";
+                }
+                else {
+                    lbclearTxt.Content += "　これより高いです";
+                }
             }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+
+            Anser = rand.Next(1, gridRow * gridColumn);
+
+            RowDefinition rowDefinition = new RowDefinition();
+            rowDefinition.Height = new GridLength(1.0, GridUnitType.Star);
+            for (int i = 0; i < gridRow; i++) {
+                grWindow.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < gridColumn; i++) {
+                grWindow.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < gridRow; i++) {
+
+                for (int j = 1; j <= gridColumn; j++) {
+                    Button bt = new Button();
+                    bt.Content = i * gridColumn + j;
+                    bt.Click += Button_Click;
+                    bt.FontSize = 20;
+                    Grid.SetRow(bt, i);
+                    Grid.SetColumn(bt, j-1);
+                    buttons.Add(bt);
+                }
+            }
+            buttons.ForEach(bt => grWindow.Children.Add(bt));
+
         }
     }
 }
